d5af54114668d44b47238e904e767ba16f809e87
mountain-agent120 <yamakun625@gmail.com>
2026-02-06 20:17:57 +0900

    Update logos_project/web/jest.config.js

---
+++ logos_project/web/jest.config.js
@@ -0,0 +1,11 @@
+const nextJest = require('next/jest')
+const createJestConfig = nextJest({ dir: './' })
+const customJestConfig = {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '\\.css$': '<rootDir>/__mocks__/styleMock.js',
+    }
+}
+module.exports = createJestConfig(customJestConfig)
